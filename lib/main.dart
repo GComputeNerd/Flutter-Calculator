b776def2e30f593e727dccdf498ecde67fa16421
GComputeNerd <GComputeNerd@users.noreply.github.com>
2023-12-02 23:41:46 +0530

    Added intial code for class to generate row of calculator buttons

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,13 +10,25 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return ChangeNotifierProvider(
+      create: (context) => CalculatorData(),
+      child: MaterialApp(
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const MyHomePage(),
       ),
-      home: const MyHomePage(),
     );
+  }
+}
+
+class CalculatorData extends ChangeNotifier {
+  var result = 0;
+
+  void updateNumber(int x) {
+    result = result*10 + x;
+    notifyListeners();
   }
 }
 
@@ -41,37 +54,68 @@ class CalculatorMain extends StatelessWidget {
     return Container(
       padding: const EdgeInsets.all(3),
       child: const Column(
+        mainAxisSize: MainAxisSize.min,
         children: [
-          CalculatorDisplay(),
-          CalculatorRow()
+           CalculatorDisplay(),
+          CalculatorRow(buttons: [
+            NumberButton(number: 1),
+            NumberButton(number: 2),
+            NumberButton(number: 3),
+          ]),
         ],
       ),
     );
   }
 }
 
-class CalculatorDisplay extends StatefulWidget {
+class CalculatorDisplay extends StatelessWidget {
   const CalculatorDisplay({super.key});
-
-  @override createState() => _CalculatorDisplayState();
-}
-
-class _CalculatorDisplayState extends State<CalculatorDisplay> {
-  var result = 0;
 
   @override
   Widget build(BuildContext context) {
-    return Text(result.toString());
+    final width = MediaQuery.of(context).size.width;
+    var appState = context.watch<CalculatorData>();
+    
+    var result = appState.result.toString();
+
+    return Container(
+      color: Colors.amber,
+      width: width*0.8,
+      margin: const EdgeInsets.symmetric(vertical: 9),
+      padding: const EdgeInsets.all(5),
+      child: Text(
+        result,
+        textAlign: TextAlign.right,),
+    );
   }
 }
 
 class CalculatorRow extends StatelessWidget {
+  final List<Widget> buttons;
+
   const CalculatorRow({
     super.key,
+    required this.buttons,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Placeholder();
+    return Row(
+      children: this.buttons
+    );
+  }
+}
+
+class NumberButton extends StatelessWidget {
+  final int? number;
+
+  const NumberButton({super.key, required this.number});
+
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<CalculatorData>();
+
+    return ElevatedButton(onPressed: () => appState.updateNumber(number!),
+    child: Text(number.toString()));
   }
 }
