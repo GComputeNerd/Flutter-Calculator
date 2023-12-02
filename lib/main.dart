06801398c1fb335cc2e2b30a4fe2b89949be6937
GComputeNerd <GComputeNerd@users.noreply.github.com>
2023-12-02 17:58:22 +0530

    Added CalculatorDisplay Stateful Widget

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,54 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Color(0xff84abb4),
+      body: CalculatorMain(),
+    );
+  }
+}
+
+class CalculatorMain extends StatelessWidget {
+  const CalculatorMain({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(3),
+      child: const Column(
+        children: [
+          CalculatorDisplay(),
+          CalculatorRow()
+        ],
+      ),
+    );
+  }
+}
+
+class CalculatorDisplay extends StatefulWidget {
+  const CalculatorDisplay({super.key});
+
+  @override createState() => _CalculatorDisplayState();
+}
+
+class _CalculatorDisplayState extends State<CalculatorDisplay> {
+  var result = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(result.toString());
+  }
+}
+
+class CalculatorRow extends StatelessWidget {
+  const CalculatorRow({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
     return Placeholder();
   }
 }
